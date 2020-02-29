0787f269910afe89956293b8695642357363e4b4
Gonzalo Sanchez Sandoval <dzgonzaloss12@gmail.com>
2020-02-29 14:04:35 +0100

    He creado el archivo del ejercicio

---
+++ funciones_json.py
@@ -0,0 +1,11 @@
+import json
+
+def listar_personajes(personajes):
+
+    lista_nombres=[]
+    lista_clase=[]
+    for charac in personajes:
+        
+        lista_titulos.append(charac["name"])
+        lista_año.append(peli["_class"])
+    return zip(lista_nombres, lista_clase)
